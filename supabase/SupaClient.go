99a9bdf056ff148910aa0e388926a42f9ca2c0fb
Matthew MacFarquhar <matthew.macfarquhar@gmail.com>
2026-01-01 12:51:16 -0500

    allow using dev env for test harness

--- supabase/SupaClient.go
+++ supabase/SupaClient.go
@@ -6,18 +6,26 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"os"
 	"strings"
 
 	"github.com/mattmacf98/buildium_harness/logger"
 )
 
 type SupaClient struct {
-	Client *http.Client
-	Token  string
+	Client  *http.Client
+	Token   string
+	BaseUrl string
+	AnonKey string
 }
 
 func NewSupaClient(ctx context.Context) *SupaClient {
-	return &SupaClient{Client: http.DefaultClient}
+	environment := os.Getenv("ENVIRONMENT")
+	if environment == "PROD" {
+		return &SupaClient{Client: http.DefaultClient, BaseUrl: "https://dpwumtpjesedslulexqz.supabase.co", AnonKey: os.Getenv("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRwd3VtdHBqZXNlZHNsdWxleHF6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjcxNDE4MzksImV4cCI6MjA4MjcxNzgzOX0.JYXW1bzTOmlCtngrlYLAbnGzRXDIcH0mDlwpbg1u8Rs")}
+	} else {
+		return &SupaClient{Client: http.DefaultClient, BaseUrl: "http://127.0.0.1:54321", AnonKey: os.Getenv("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0")}
+	}
 }
 
 func (c *SupaClient) AddProjectRun(ctx context.Context, projectId string, stage int, logs []logger.Log) (*http.Response, error) {
@@ -25,13 +33,13 @@ func (c *SupaClient) AddProjectRun(ctx context.Context, projectId string, stage 
 	if err != nil {
 		return nil, err
 	}
-	req, err := http.NewRequestWithContext(ctx, "POST", "https://dpwumtpjesedslulexqz.supabase.co/functions/v1/create-project-run",
+	req, err := http.NewRequestWithContext(ctx, "POST", c.BaseUrl+"/functions/v1/create-project-run",
 		strings.NewReader(fmt.Sprintf(`{"projectId":"%s", "stage":%d, "logsJson":%s, "token":"%s"}`, projectId, stage, logsJson, c.Token)))
 	if err != nil {
 		return nil, err
 	}
 	req.Header.Set("x-buildium-token", c.Token)
-	req.Header.Set("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRwd3VtdHBqZXNlZHNsdWxleHF6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjcxNDE4MzksImV4cCI6MjA4MjcxNzgzOX0.JYXW1bzTOmlCtngrlYLAbnGzRXDIcH0mDlwpbg1u8Rs")
+	req.Header.Set("Authorization", "Bearer "+c.AnonKey)
 	req.Header.Set("Content-Type", "application/json")
 
 	resp, err := c.Client.Do(req)
@@ -42,12 +50,12 @@ func (c *SupaClient) AddProjectRun(ctx context.Context, projectId string, stage 
 }
 
 func (c *SupaClient) Login(ctx context.Context, email string, password string) error {
-	req, err := http.NewRequestWithContext(ctx, "POST", "https://dpwumtpjesedslulexqz.supabase.co/functions/v1/login",
+	req, err := http.NewRequestWithContext(ctx, "POST", c.BaseUrl+"/functions/v1/login",
 		strings.NewReader(fmt.Sprintf(`{"email":"%s", "password":"%s"}`, email, password)))
 	if err != nil {
 		return err
 	}
-	req.Header.Set("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRwd3VtdHBqZXNlZHNsdWxleHF6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjcxNDE4MzksImV4cCI6MjA4MjcxNzgzOX0.JYXW1bzTOmlCtngrlYLAbnGzRXDIcH0mDlwpbg1u8Rs")
+	req.Header.Set("Authorization", "Bearer "+c.AnonKey)
 	req.Header.Set("Content-Type", "application/json")
 
 	resp, err := c.Client.Do(req)
